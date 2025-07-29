9c77bd77493f58ac4839361844e3caa5c5e79946
alec <ajcuccia@gmail.com>
2025-07-28 22:50:17 -0400

    Update mailto email

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import monkeypatchLogo from './images/logo_01_full.png';
 import './styles/style.css';
 
 document.querySelector('#app').innerHTML = `
-  <a href="mailto:monkeypatchsolutions@purelymail.com">
+  <a href="mailto:info@monkeypatchsolutions.com">
     <img src="${monkeypatchLogo}" class="logo" alt="Monkeypatch Solutions" />
   </a>
 `;
