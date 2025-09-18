862b2d4f3aedd46339f8409f5706815614ba0f57
alec <alec.cuccia@pm.me>
2025-09-17 21:03:44 -0400

    Make nav not suck

--- src/main.js
+++ src/main.js
@@ -1,27 +1,86 @@
 // let currentSection = 0;
 let lastScrollTop = 0;
 
+// TODO:
+// Overflow on header over scroll bar
+// Add and talk about the copy from discord
+// Potentially update link verbage and appearance
+// Add a contact us button that will send an email to our work accounts
+// Also, we have to make sure that we are WCAGG 2.1 AA compliant
+
 const main = document.getElementsByTagName('main')[0];
 if (main) {
+  // Interface elements
+  const scrollBtn = document.getElementById('scroll-btn');
+
+  // Container elements
+  const hero = document.getElementById('hero');
+  const header = main.getElementsByTagName('header')[0];
+  const sections = main.getElementsByTagName('section');
+  const nav = main.getElementsByTagName('nav')[0];
+  const navBtn = document.getElementById('nav-btn');
+
+  const scrolledDownThePageThreshold = main.clientHeight - (main.clientHeight / 2);
+
+  const shouldShowNav = () => {
+    if (main.scrollTop >= scrolledDownThePageThreshold) {
+      nav.classList.remove('hide');
+    }
+  }
+
+  shouldShowNav();
+  if (window.innerWidth < 769) { // mobile
+    nav.style.top = `-${nav.clientHeight}px`;
+  } else { // desktop
+    nav.style.top = `${header.clientHeight}px`;
+  }
+  addEventListener("resize", () => {
+    shouldShowNav();
+    if (window.innerWidth < 769) { // mobile
+      if (nav.classList.contains('hide')) {
+        nav.style.top = `-${nav.clientHeight}px`;
+      }
+    } else { // desktop
+      nav.style.top = `${header.clientHeight}px`;
+    }
+  });
+
   // If page loads on a section show header immediately
   if (main.scrollTop >= main.clientHeight) {
     main.getElementsByTagName('header')[0].classList.add('scrolled');
   }
 
-  const sections = main.getElementsByTagName("section");
+  // Key Mappings
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+      e.preventDefault();
+      const direction = e.key === 'ArrowUp' ? -1 : 1;
+      main.scrollBy({ top: direction * window.innerHeight, behavior: 'smooth' });
+    }
+  });
+
+  // Scroll #accessibility into view on `V` button click
+  scrollBtn.addEventListener('click', () => {
+    document.getElementById('accessibility').scrollIntoView({ behavior: 'smooth' });
+  })
+
   main.addEventListener('scroll', () => {
     // Show or hide the header depending on scroll position
     if (main.scrollTop > lastScrollTop) { // Scrolling down
-      if (main.scrollTop >= main.clientHeight - (main.clientHeight / 2)) {
-        document.getElementById('hero').classList.add('scrolled');
-        main.getElementsByTagName('header')[0].classList.add('scrolled', 'transition');
-        main.getElementsByTagName('header')[0].classList.remove('quick');
+      if (main.scrollTop >= scrolledDownThePageThreshold) {
+        hero.classList.add('scrolled');
+        scrollBtn.classList.add('scrolled');
+        header.classList.add('scrolled', 'transition');
+        header.classList.remove('quick');
+        nav.classList.remove('hide');
       }
     } else if (main.scrollTop < lastScrollTop) { // Scrolling up
-      if (main.scrollTop <= main.clientHeight - (main.clientHeight / 2)) {
-        document.getElementById('hero').classList.remove('scrolled');
-        main.getElementsByTagName('header')[0].classList.remove('scrolled', 'transition');
-        main.getElementsByTagName('header')[0].classList.add('quick');
+      if (main.scrollTop <= scrolledDownThePageThreshold) {
+        hero.classList.remove('scrolled');
+        scrollBtn.classList.remove('scrolled');
+        header.classList.remove('scrolled', 'transition');
+        header.classList.add('quick');
+        nav.classList.add('hide');
       }
     }
 
@@ -52,15 +111,15 @@ if (main) {
   }, { passive: true });
 
   // Toggle mobile nav
-  const nav = main.getElementsByTagName('nav')[0];
-  const navBtn = document.getElementById('nav-btn');
-  navBtn.addEventListener('click', (e) => {
+  navBtn.addEventListener('click', () => {
+    const headerHeight = header.clientHeight;
+    const navHeight = nav.clientHeight;
     if (navBtn.classList.contains('show')) {
       navBtn.classList.remove('show');
-      nav.classList.remove('show');
+      nav.style.top = `-${navHeight}px`;
     } else {
       navBtn.classList.add('show');
-      nav.classList.add('show');
+      nav.style.top = `${headerHeight}px`;
     }
   }, false);
 
