16cedd7fe831e513b7ca0187e6500b1c6e82f306
alec <alec.cuccia@pm.me>
2025-09-18 21:15:23 -0400

    More fixes

--- src/main.js
+++ src/main.js
@@ -7,34 +7,36 @@ let lastScrollTop = 0;
 // Potentially update link verbage and appearance
 // Add a contact us button that will send an email to our work accounts
 // Also, we have to make sure that we are WCAGG 2.1 AA compliant
+// nav menu doesn't close when you click nav link
+//  tried to do it but then the setHide in shouldShowNav in the scroll listener kept it open sometimes
+//  then i noticed that the scroll listener will close it sometimes
 
-const main = document.getElementsByTagName('main')[0];
-if (main) {
+const body = document.getElementsByTagName('body')[0];
+if (body) {
   // Interface elements
   const scrollBtn = document.getElementById('scroll-btn');
 
   // Container elements
   const hero = document.getElementById('hero');
-  const header = main.getElementsByTagName('header')[0];
-  const sections = main.getElementsByTagName('section');
-  const nav = main.getElementsByTagName('nav')[0];
+  const header = body.getElementsByTagName('header')[0];
+  const sections = body.getElementsByTagName('section');
+  const nav = body.getElementsByTagName('nav')[0];
   const navBtn = document.getElementById('nav-btn');
 
-  const scrolledDownThePageThreshold = main.clientHeight - (main.clientHeight / 2);
-  
-    const setHide = (element, status) => {
-      if (status) {
-        element.classList.remove('hide');
-        element.hidden = true;
-      } else {
-        element.classList.add('hide');
-        element.hidden = false;
-      }
+  const scrolledDownThePageThreshold = body.clientHeight - (body.clientHeight / 2);
+
+  const setHide = (element, status) => {
+    element.hidden = status;
+    if (status) {
+      element.classList.add('hide');
+    } else {
+      element.classList.remove('hide');
     }
+  }
 
   const shouldShowNav = () => {
-    if (main.scrollTop >= scrolledDownThePageThreshold) {
-      setHide(nav, true);
+    if (body.scrollTop >= scrolledDownThePageThreshold) {
+      setHide(nav, false);
     }
   }
 
@@ -47,7 +49,7 @@ if (main) {
   addEventListener("resize", () => {
     shouldShowNav();
     if (window.innerWidth < 769) { // mobile
-      if (nav.classList.contains('hide')) {
+      if (nav.classList.contains('hide') || !navBtn.classList.contains('show')) {
         nav.style.top = `-${nav.clientHeight}px`;
       }
     } else { // desktop
@@ -56,8 +58,8 @@ if (main) {
   });
 
   // If page loads on a section show header immediately
-  if (main.scrollTop >= main.clientHeight) {
-    main.getElementsByTagName('header')[0].classList.add('scrolled');
+  if (body.scrollTop >= body.clientHeight) {
+    body.getElementsByTagName('header')[0].classList.add('scrolled');
   }
 
   // Key Mappings
@@ -65,7 +67,7 @@ if (main) {
     if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
       e.preventDefault();
       const direction = e.key === 'ArrowUp' ? -1 : 1;
-      main.scrollBy({ top: direction * window.innerHeight, behavior: 'smooth' });
+      body.scrollBy({ top: direction * window.innerHeight, behavior: 'smooth' });
     }
   });
 
@@ -74,50 +76,50 @@ if (main) {
     document.getElementById('accessibility').scrollIntoView({ behavior: 'smooth' });
   })
 
-  main.addEventListener('scroll', () => {
+  body.addEventListener('scroll', () => {
     // Show or hide the header depending on scroll position
-    if (main.scrollTop > lastScrollTop) { // Scrolling down
-      if (main.scrollTop >= scrolledDownThePageThreshold) {
+    if (body.scrollTop > lastScrollTop) { // Scrolling down
+      if (body.scrollTop >= scrolledDownThePageThreshold) {
         hero.classList.add('scrolled');
         scrollBtn.classList.add('scrolled');
         header.classList.add('scrolled', 'transition');
         header.classList.remove('quick');
-        setHide(nav, true);
+        setHide(nav, false);
       }
-    } else if (main.scrollTop < lastScrollTop) { // Scrolling up
-      if (main.scrollTop <= scrolledDownThePageThreshold) {
+    } else if (body.scrollTop < lastScrollTop) { // Scrolling up
+      if (body.scrollTop <= scrolledDownThePageThreshold) {
         hero.classList.remove('scrolled');
         scrollBtn.classList.remove('scrolled');
         header.classList.remove('scrolled', 'transition');
         header.classList.add('quick');
-        setHide(nav, false);
+        setHide(nav, true);
       }
     }
 
     // Update location hash based on scroll position
-    if (main.scrollTop === 0) {
+    if (body.scrollTop === 0) {
       location.hash = '';
-    } else if (main.scrollTop === main.clientHeight) {
+    } else if (body.scrollTop === body.clientHeight) {
       location.hash = sections[1].id;
-    } else if (main.scrollTop === main.clientHeight * 2) {
+    } else if (body.scrollTop === body.clientHeight * 2) {
       location.hash = sections[2].id;
-    } else if (main.scrollTop === main.clientHeight * 3) {
+    } else if (body.scrollTop === body.clientHeight * 3) {
       location.hash = sections[3].id;
     }
 
     // // Keep track of current section
-    // if (main.scrollTop >= main.clientHeight * 3) {
+    // if (body.scrollTop >= body.clientHeight * 3) {
     //   currentSection = 3;
-    // } else if (main.scrollTop >= main.clientHeight * 2) {
+    // } else if (body.scrollTop >= body.clientHeight * 2) {
     //   currentSection = 2;
-    // } else if (main.scrollTop >= main.clientHeight) {
+    // } else if (body.scrollTop >= body.clientHeight) {
     //   currentSection = 1
-    // } else if (main.scrollTop === 0) {
+    // } else if (body.scrollTop === 0) {
     //   currentSection = 0;
     // }
 
     // Update last scroll position
-    lastScrollTop = main.scrollTop <= 0 ? 0 : main.scrollTop;
+    lastScrollTop = body.scrollTop <= 0 ? 0 : body.scrollTop;
   }, { passive: true });
 
   // Toggle mobile nav
@@ -127,19 +129,22 @@ if (main) {
     if (navBtn.classList.contains('show')) {
       navBtn.classList.remove('show');
       nav.style.top = `-${navHeight}px`;
+      nav.hidden = true;
     } else {
       navBtn.classList.add('show');
       nav.style.top = `${headerHeight}px`;
+      nav.hidden = false;
     }
   }, false);
 
   // Add click events to nav bar buttons
-  [...main.getElementsByTagName('button')].forEach((button) => {
+  [...body.getElementsByTagName('button')].forEach((button) => {
     button.addEventListener('click', () => {
       if (button.name) {
         document.getElementById(button.name).scrollIntoView({ behavior: 'smooth' });
         navBtn.classList.remove('show');
-        nav.classList.remove('show');
+        // nav.classList.remove('show');
+        setHide(nav, true);
       }
     });
   });
